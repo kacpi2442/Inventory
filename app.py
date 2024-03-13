08d07331661d583ab0cea6821f74f2efc4bd51fa
kacpi2442 <git@2442.pl>
2024-03-13 21:12:55 +0100

    Update deployment configuration and database initialization

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ async def run_flask_app():
     app.run()
 
 if __name__ == '__main__':
-    #db.create_all()
     with app.app_context():
+        db.create_all()
         with Session(db.engine) as session:
             run_bot_threaded(session)
     app.run(use_reloader=False)
